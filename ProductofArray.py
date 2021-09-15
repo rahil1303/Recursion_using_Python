ddc75dbbe2924a3bd73763034d18d8c71ab26ca3
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-15 16:49:25 +0530

    Update ProductofArray.py

--- ProductofArray.py
+++ ProductofArray.py
@@ -2,6 +2,6 @@ def productsofArray(n):
   if len(arr) == 0:
     return 1
   else:
-    return arr[0] * productsofArray([1::])
+    return arr[0] * productsofArray(arr[1::])
   
  print(productsofArray([1,2,3))
